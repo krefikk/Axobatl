1c248426840b5d353dfc7a9d9a6b9da49fde1f1a
Ahmet Refik Şener <ahmetrefikks@gmail.com>
2024-05-20 15:35:36 +0300

    Removed Tutorial Checkbox

--- Assets/Scripts/UI/SettingsMenuHandler.cs
+++ Assets/Scripts/UI/SettingsMenuHandler.cs
@@ -11,17 +11,14 @@ public class SettingsMenuHandler : MonoBehaviour
     
     public Slider sfxSlider;
     public Slider musicSlider;
-    public Toggle checkBox;
     public TextMeshProUGUI musicValue;
     public TextMeshProUGUI sfxValue;
     public Animator backAnim;
     public Animator titleAnim;
     public Animator musicTextAnim;
     public Animator sfxTextAnim;
-    public Animator tutorialTextAnim;
     public Animator musicSliderAnim;
     public Animator sfxSliderAnim;
-    public Animator checkBoxAnim;
     public Animator musicValueAnim;
     public Animator sfxValueAnim;
 
@@ -34,7 +31,6 @@ public class SettingsMenuHandler : MonoBehaviour
     {
         sfxSlider.value = wholeGameManager.GetSFXVolume() / 100;
         musicSlider.value = wholeGameManager.GetMusicVolume() / 100;
-        checkBox.isOn = wholeGameManager.IsTutorialsOn();
     }
 
     private void Update()
@@ -80,16 +76,6 @@ public class SettingsMenuHandler : MonoBehaviour
     public void SFXTextExitAnimation()
     {
         sfxTextAnim.Play("sfxVolumeTextExit");
-    }
-
-    public void TutorialTextEnterAnimation()
-    {
-        tutorialTextAnim.Play("tutorialsEnter");
-    }
-
-    public void TutorialTextExitAnimation()
-    {
-        tutorialTextAnim.Play("tutorialsExit");
     }
 
     public void TitleEnterAnimation()
@@ -142,16 +128,6 @@ public class SettingsMenuHandler : MonoBehaviour
         sfxSliderAnim.Play("sfxSliderExit");
     }
 
-    public void CheckBoxEnterAnimation()
-    {
-        checkBoxAnim.Play("checkBoxEnter");
-    }
-
-    public void CheckBoxExitAnimation()
-    {
-        checkBoxAnim.Play("checkBoxExit");
-    }
-
     public void BackEnterAnimation() 
     {
         backAnim.Play("backEnter");
@@ -170,10 +146,7 @@ public class SettingsMenuHandler : MonoBehaviour
     {
         sfxSlider.interactable = false;
         musicSlider.interactable = false;
-        checkBox.interactable = false;
         BackExitAnimation();
-        yield return new WaitForSeconds(0.25f);
-        CheckBoxExitAnimation();
         yield return new WaitForSeconds(0.25f);
         SFXSliderExitAnimation();
         yield return new WaitForSeconds(0.25f);
@@ -183,8 +156,6 @@ public class SettingsMenuHandler : MonoBehaviour
         yield return new WaitForSeconds(0.25f);
         MusicValueExitAnimation();
         yield return new WaitForSeconds(0.25f);
-        TutorialTextExitAnimation();
-        yield return new WaitForSeconds(0.25f);
         SFXTextExitAnimation();
         yield return new WaitForSeconds(0.25f);
         MusicTextExitAnimation();
@@ -193,7 +164,6 @@ public class SettingsMenuHandler : MonoBehaviour
         yield return new WaitForSeconds(0.25f);
         sfxSlider.interactable = true;
         musicSlider.interactable = true;
-        checkBox.interactable = true;
         if (PlayerController.player is not null) 
         {
             SceneManager.LoadScene("MainGame");
